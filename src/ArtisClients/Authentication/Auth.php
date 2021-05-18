fa0627f755494c0a4d6fe0bbca3ec05a778ca715
Uday <Uday@DESKTOP-IKI0RGU>
2021-05-18 16:10:48 +0530

    First commit

---
+++ src/ArtisClients/Authentication/Auth.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+namespace Archisys\Ecom;
+/**
+ * Description of Auth
+ *
+ * @author Uday
+ */
+class Auth {
+    
+    public function greet($greet = "Hello World")
+    {
+        return $greet;
+    }
+}
