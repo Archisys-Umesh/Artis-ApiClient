ba480b0513393faba7345b87ea61bc2e970e7ad2
Uday <Uday@DESKTOP-IKI0RGU>
2021-05-18 16:36:46 +0530

    Artis client

--- src/ArtisClients/Authentication/Auth.php
+++ src/ArtisClients/Authentication/Auth.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-namespace Archisys\Ecom\ArtisClients\Authentication;
+namespace Archisys\Ecom\Authentication;
 /**
  * Description of Auth
  *
