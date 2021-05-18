259874935239f7cde43d42824229c9426863b042
Uday <Uday@DESKTOP-IKI0RGU>
2021-05-18 16:34:07 +0530

    First commit

--- src/ArtisClients/Authentication/Auth.php
+++ src/ArtisClients/Authentication/Auth.php
@@ -5,7 +5,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-namespace Archisys\Ecom;
+namespace Archisys\Ecom\ArtisClients\Authentication;
 /**
  * Description of Auth
  *
